b602bc63721450126a7aeaefa59e09072b12ce13
kisshc <kisshc@kisshc.com>
2015-08-13 06:15:42 -0400

    default controller and action

--- conf/config.php
+++ conf/config.php
@@ -1,13 +1,16 @@
 <?php
 $config = array();
-$config["db"]['type']			=	'mysql';
-$config["db"]['host']			=	'172.16.1.63';
-$config["db"]['database']		=	'spider';
-$config["db"]['username']		=	'spider';
-$config["db"]['password']		=	'123456';
-$config["db"]['charset']		=	'utf8';
-$config["db"]['collation']		=	'utf8_unicode_ci';
-$config["db"]['prefix']			=	'';
+$config['db']['type']			=	'mysql';
+$config['db']['host']			=	'172.16.1.63';
+$config['db']['database']		=	'spider';
+$config['db']['username']		=	'spider';
+$config['db']['password']		=	'123456';
+$config['db']['charset']		=	'utf8';
+$config['db']['collation']		=	'utf8_unicode_ci';
+$config['db']['prefix']			=	'';
 
-$config["view"]['path'] = "app/view/";
-$config["view"]['cache'] = "app/cache/";
+$config['view']['path'] = 'app/view/';
+$config['view']['cache'] = 'app/cache/';
+
+$config['app']['default']['controller_name'] = "index";
+$config['app']['default']['action_name'] = "index";
--- core/init.php
+++ core/init.php
@@ -4,18 +4,34 @@ namespace system;
 class init{
 	public static function start(){
 		$request = new request;
-		//这里只是简单的过去控制器及操作方法的名称
-		$controller = $request -> fetch("get.c") . "Controller";
-		$controller_namespace = "app\\controller\\";
-		$controller_class = $controller_namespace . $controller;
-		$method = $request -> fetch("get.a");
+		$controller_namespace = "app\\controller\\";	
+		//默认 action
+		if($request -> IS_GET){
+			$controller = $request -> fetch("get.c");
+			$action = $request -> fetch("get.a");
+		}
+		if($request -> IS_POST){
+			$controller = $request -> fetch("post.c");
+			$action = $request -> fetch("post.a");			
+		}
 		
-		if(!class_exists($controller_class))
-			throw new exception("{$controller_class} : Controller not found");
+		$config = C("app.default");
+		if(!$controller){		
+			$controller = $config['controller_name'];
+		}
+		if(!$action){
+			$action = $config['action_name'];
+		}
 		
-		if(!method_exists($controller_class,$method))
-			throw new exception("{$method} : Method not found on {$controller_class}");
+		$controller = $controller_namespace . $controller . "Controller";		
 		
-		call_user_func_array(array(new $controller_class,$method),array($request));
+		if(!class_exists($controller))
+			throw new exception("{$controller} : Controller not found");
+		
+		if(!method_exists($controller,$action))
+			throw new exception("{$action} : action not found on {$controller}");
+		
+		call_user_func_array(array(new $controller_class,$action),array($request));
 	}
+	
 }
